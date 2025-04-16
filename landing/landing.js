94eb80df5a0b3cf0c92bbeb3db1e4fb2c875ee6e
Akhirah Strong <codeclubdetroit@gmail.com>
2025-04-16 19:08:38 -0400

    add Stripe to front end backend

--- landing/landing.js
+++ landing/landing.js
@@ -1,7 +1,7 @@
 const stripe = Stripe("pk_test_51RDmW7HUjZD2YXOR0c5P3PJjahcMtGkDS3ubMSTi0wDxCTFGs1JIMHxHpXyvaEQiS9AJMXvWLKDh2rjI2wLTAcZn00ZXTkQ1dr");
 
 async function checkout(priceId) {
-  const res = await fetch("https://your-backend.com/create-checkout-session", {
+  const res = await fetch("https://b8df0ca0-33e6-4b75-a1f3-5524ede8a8a3-00-311caz7ousjf5.kirk.replit.dev/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ priceId }),
