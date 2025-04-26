9d495818775306062ae5e26675aac754c66b8455
Akhirah Strong <codeclubdetroit@gmail.com>
2025-04-25 20:59:22 -0400

    debug

--- localLimit.js
+++ localLimit.js
@@ -11,11 +11,11 @@ document.addEventListener("DOMContentLoaded", () => {
     window.open("https://buy.stripe.com/test_5kA18195J10kdr28ww", "_blank");
   });
 
-  // document.getElementById("signUpBtn").addEventListener("click", () => {
-  //   chrome.tabs.create({
-  //     url: chrome.runtime.getURL("signup.html")
-  //   });
-  // });
+  document.getElementById("signUpBtn").addEventListener("click", () => {
+    chrome.tabs.create({
+      url: chrome.runtime.getURL("signup.html")
+    });
+  });
 
   // document.getElementById("loginBtn").addEventListener("click", () => {
   //   window.open("https://your-site.com/login", "_blank");
@@ -27,14 +27,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
  
   // Login button function
-  document.getElementById("signUpBtn").addEventListener("click", () => {
-    chrome.windows.create({
-      url: chrome.runtime.getURL("signup.html"),
-      type: "popup",
-      width: 500,
-      height: 600
-    });
-  });
+  // document.getElementById("signUpBtn").addEventListener("click", () => {
+  //   chrome.windows.create({
+  //     url: chrome.runtime.getURL("signup.html"),
+  //     type: "popup",
+  //     width: 500,
+  //     height: 600
+  //   });
+  // });
   
 
 
